aceab90f72342b5891e00f2ca9db081b264c30ad
arion <53127594+JustArion@users.noreply.github.com>
2024-01-05 18:46:56 +0200

    Vanilla GUI Fix: Star Crux bar digits would underlap on the points icon

---
+++ StarCruxExpansion/Harmony/UI/Over3DigitsStarCruxBarTidy_Patch.cs
@@ -0,0 +1,22 @@
+﻿namespace Dawn.DMD.StarCruxExpansion.Harmony;
+
+using Claw.Core.Utils;
+using Death.TimesRealm.UserInterface.Darkness;
+using JetBrains.Annotations;
+using Reflection;
+
+[HarmonyPatch(typeof(GUI_DarknessBar), nameof(GUI_DarknessBar.UpdateDisplay))]
+public class Over3DigitsStarCruxBarTidy_Patch
+{
+    [UsedImplicitly]
+    private static void Postfix(GUI_DarknessBar __instance)
+    {
+        var totalPoints = __instance.Controller().TotalPoints;
+
+        if (totalPoints >= 100)
+        {
+            __instance.transform.Find("Icon_Points")
+                .SetLocalPosX(405); // This shifts it more to the right, so that the third digit can show properly.
+        }
+    }
+}
---
+++ StarCruxExpansion/Reflection/GUI_DarknessBarReflection.cs
@@ -0,0 +1,12 @@
+﻿namespace Dawn.DMD.StarCruxExpansion.Reflection;
+
+using Death.Darkness;
+using Death.TimesRealm.UserInterface.Darkness;
+using ReflectionHelpers;
+
+public static class GUI_DarknessBarReflection
+{
+    private static readonly Lazy<Func<GUI_DarknessBar, IDarknessController>> _controller = PrivateFieldsHelper.CreateFieldGetterDelegate<GUI_DarknessBar, IDarknessController>(nameof(_controller));
+
+    public static IDarknessController Controller(this GUI_DarknessBar challenge) => _controller.Value(challenge);
+}
